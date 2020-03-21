acc497e6aa775ef3c94df63a7a5743db9c4a2cc6
Anton Saushkin <saushkin_anton@mail.ru>
2020-03-21 12:00:37 +0300

    First commit

---
+++ VkGroupStat/src/test/java/com/vkgroupstat/VkGroupStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkgroupstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkGroupStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
